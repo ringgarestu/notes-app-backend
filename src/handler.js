ea5b1805cb1ebc0402b8c7265e3c08918a65b405
ringgavertika <ringgarestu@gmail.com>
2023-07-21 17:44:21 +0700

    added: fitur-menampilkan-catatan

--- src/handler.js
+++ src/handler.js
@@ -52,7 +52,7 @@ const getAllNotesHandler = () => ({
 const getNoteByIdHandler = (request, h) => {
   const { id } = request.params;
 
-  const note = notes.filter((n) => n.id === id[0]);
+  const note = notes.filter((n) => n.id === id)[0];
 
   if (note !== undefined) {
     return {
